25319f6be46f9a1a6b5360f160cc2e2116309690
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-02-17 14:39:14 +0530

    Finish Untill Header

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header_content">
+            <h2>Order Your favorite food Here</h2>
+            <p>
+                Choose from a diverse menu featuring a delectable array of deishes crafted with the finest ingredient and culinary expertise.satisfy your cravings and elevate your dining experience,one delicious meal at a time.
+            </p>
+            <button>View Menu</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
