604954ddeb3575601b163b129991868b93f25afa
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-02-19 17:52:18 +0530

    Create and done the image adding functions frontend in the dishes images

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { useState } from 'react'
 
 const FoodItem = ({id,name,price,description,image}) => {
 
+  const [itemCount, setItemCount] = useState(0)
 
 
   return (
     <div className="foodItem">
         <div className="foodItem_Image_container">
             <img className='foodItem_Image' src={image} alt="" />
+            {!itemCount
+                ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" srcset="" />
+                :<div className="fooditem_counter">
+                  <img src={assets.remove_icon_red} onClick={()=>setItemCount(prev=>prev-1)} alt="" />
+                  <p>{itemCount}</p>
+                  <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                </div>
+            
+            }
         </div>
         <div className="foodItem_Info">
             <div className="foodItem_name_rating">
