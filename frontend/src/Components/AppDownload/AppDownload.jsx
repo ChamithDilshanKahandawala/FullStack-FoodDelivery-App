22bbbfc59acf2bf39785f4ed44a94b3dda47af47
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-02-20 21:30:56 +0530

    Add the AppDownload component with css properties

---
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets';
+
+const AppDownload = () => {
+  return (
+    <div className="appDownload " id='AppDownload'>
+        <p>
+            For Better Experience Download <br />Tomato App 
+        </p>
+        <div className="app_download_platforms">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default AppDownload;
