edebec1b7eae8c90cc89d192c6f9ef14c7adb112
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-02-20 20:03:18 +0530

    Create if condtion for category based item showing

--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -13,7 +13,12 @@ const FoodDisplay = ({category}) => {
         <h2>Top Dishes Near You</h2>
         <div className="foodDisplay_List">
             {food_list.map((item,index)=>{
+              {console.log(category,item.category);}
+              
+              if (category ==="All" || category===item.category) {
                 return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image} />
+              }
+                
 
             })}
         </div>
