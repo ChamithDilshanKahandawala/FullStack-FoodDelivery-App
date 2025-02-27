00ef2590d88a19a4aab5731586b923b516ca1117
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-02-27 23:26:50 +0530

    Add Logic to the dot class if item in cart show it in orange dot in cart imge and if not don't show this dot

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import {Link} from 'react-router-dom'
+import { StoreContext } from '../../Context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
+
     const [menu,setMenu] =useState("home");
+
+    const {getTotalCartAmmount} = useContext(StoreContext)
+
+
   return (
     <div className="navbar">
         <Link to='/'><img src={assets.logo} className='logo'/></Link>
@@ -20,7 +26,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="" />
             <div className="navbar_searchIcon">
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                <div className="dot">
+                <div className={getTotalCartAmmount()===0?"":"dot"}>
 
                 </div>
             </div>
